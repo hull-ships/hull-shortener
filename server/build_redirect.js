abce67d7bc4f76ea975fe0dee393ba321eff8d5b
Romain Dardour <romain@hull.io>
2020-01-17 12:33:45 +0100

    fix https redirect

--- server/build_redirect.js
+++ server/build_redirect.js
@@ -2,13 +2,9 @@ import { extract, parse, stringify } from "query-string";
 
 const redirect = ({ organization, long_url, query = {}, referrer = "" }) => {
   const dest_qs = stringify({ ...parse(extract(long_url)), ...query });
-  const long = long_url.match(/^http(s)?:\/\//)
-    ? long_url
-    : `https://${long_url}`;
-  const url = dest_qs ? `${long.split("?").shift()}?${dest_qs}` : long;
+  const url = dest_qs ? `${long_url.split("?").shift()}?${dest_qs}` : long_url;
   const qs = {
-    ...query,
-    url
+    ...query
   };
   if (referrer) qs.referrer = referrer;
   return `${url}?${stringify(qs)}`;
