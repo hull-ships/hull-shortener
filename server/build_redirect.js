c7a5775aa1b58afa770e020b5d07e6b4378a01f9
Romain Dardour <romain@hull.io>
2020-01-17 12:30:16 +0100

    don't rely on hull redirect anymore

--- server/build_redirect.js
+++ server/build_redirect.js
@@ -2,14 +2,16 @@ import { extract, parse, stringify } from "query-string";
 
 const redirect = ({ organization, long_url, query = {}, referrer = "" }) => {
   const dest_qs = stringify({ ...parse(extract(long_url)), ...query });
-  const long = (long_url.match(/^http(s)?:\/\//)) ? long_url : `https://${long_url}`;
-  const url = (dest_qs) ? `${long.split("?").shift()}?${dest_qs}` : long;
+  const long = long_url.match(/^http(s)?:\/\//)
+    ? long_url
+    : `https://${long_url}`;
+  const url = dest_qs ? `${long.split("?").shift()}?${dest_qs}` : long;
   const qs = {
     ...query,
     url
   };
   if (referrer) qs.referrer = referrer;
-  return `https://${organization}/r?${stringify(qs)}`;
+  return `${url}?${stringify(qs)}`;
 };
 
 export default redirect;
