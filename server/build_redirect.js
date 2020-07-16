64f42bdf810ff0a40ba73d613ddfdb862687fa9f
Andy <andy@hull.io>
2020-07-16 11:58:54 -0400

    redirect to website connector

--- server/build_redirect.js
+++ server/build_redirect.js
@@ -1,13 +1,20 @@
 import { extract, parse, stringify } from "query-string";
 
-const redirect = ({ organization, long_url, query = {}, referrer = "" }) => {
+const redirect = ({ ship, organization, long_url, query = {}, referrer = "" }) => {
+  const [namespace] = organization.split(".");
   const dest_qs = stringify({ ...parse(extract(long_url)), ...query });
   const url = dest_qs ? `${long_url.split("?").shift()}?${dest_qs}` : long_url;
   const qs = {
     ...query
   };
   if (referrer) qs.referrer = referrer;
-  return `${url}?${stringify(qs)}`;
+
+  const websiteQs = {
+    url: `${url}?${stringify(qs)}`,
+    "hull-app-id": ship
+  }
+
+  return `https://${namespace}.web.hullapp.io/api/v1/r?${stringify(websiteQs)}`;
 };
 
 export default redirect;
