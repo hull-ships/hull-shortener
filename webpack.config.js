69298ff7282d18774b4cfd4ee113242b90b256ff
Romain Dardour <romain@hull.io>
2021-02-24 20:09:48 +0100

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var path = require("path");
-var webpack = require("webpack");
-var plugins = [];
+const path = require("path");
+const webpack = require("webpack");
+let plugins = [];
 
 if (process.env.NODE_ENV === "production") {
   plugins = [
@@ -11,43 +11,49 @@ if (process.env.NODE_ENV === "production") {
       }
     }),
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-    })
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
   ];
 }
 
 module.exports = {
   devtool: "#source-map",
   entry: {
-    admin: path.join(__dirname, "src/index.js"),
+    admin: path.join(__dirname, "src/index.js")
   },
   output: {
     path: path.join(__dirname, "/dist/"),
     filename: "[name].js",
-    publicPath: "/"
+    publicPath: "/",
   },
   plugins,
-  resolve: { extensions: ["", ".js", ".jsx"] },
+  resolve: { extensions: [".js", ".jsx"] },
   module: {
-    loaders: [
-      { test: /\.(css|scss)$/, loaders: ["style?singleton=true", "css?modules&importLoaders=1"] },
-      { test: /\.svg$/, loader: "svg-inline" },
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        loaders: [
+          "style-loader?singleton=true",
+          "css-loader?modules&importLoaders=1"
+        ],
+      },
+      { test: /\.svg$/, loader: "svg-inline-loader" },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel",
+        loader: "babel-loader",
         query: {
           presets: ["es2015", "stage-0"]
-        }
+        },
       },
       {
         test: /.jsx?$/,
-        loader: "babel",
+        loader: "babel-loader",
         exclude: /node_modules/,
         query: {
           presets: ["es2015", "react", "stage-0"]
-        }
-      }
-    ]
-  }
+        },
+      },
+    ],
+  },
 };
